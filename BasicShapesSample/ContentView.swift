756b978864bab3d6382fc686c294e1329ee895a6
SamplesMaker <insert@kakao.com>
2020-02-20 23:45:51 +0900

    SwiftUI Basic Shape Sample

--- BasicShapesSample/ContentView.swift
+++ BasicShapesSample/ContentView.swift
@@ -8,9 +8,44 @@
 
 import SwiftUI
 
+
+// 기본 도형그리기 샘플
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            // 정사각형
+            Rectangle()
+                .stroke(Color.black, lineWidth: 10)
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            // 동그라미
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            // 캡슐모양
+            Capsule()
+                .fill(Color.pink)
+                .overlay(Capsule().stroke(Color.black, lineWidth: 20))
+                .frame(width: 300, height: 100)
+                .padding()
+
+            // 라운딩 처리가 된 정사각형
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.yellow)
+                .frame(width: 200, height: 200)
+                .padding()
+
+            
+        }.padding()
+        
+        
+        
+        
     }
 }
 
